beffa04a1d2dabed5ca2a2d10f8c1d6181386e86
aman219 <65351629+aman219@users.noreply.github.com>
2025-01-04 09:03:00 +0530

    Added : user module implimented User module is implimented and testing is pending perttierrc added for uniform code

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,4 +1,4 @@
-export const options = {
+export const OPTIONS = {
     httpOnly: true,
     secure: false,
 };
--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import upload from "../middlewares/multer.js";
-
+import auth from "../middlewares/auth.js";
 export const router = Router();
 
-import { test, registerUser } from "../controllers/user.js";
+import { test, registerUser, login, logout, get, edittext, editimage, editpassword } from "../controllers/user.js";
 
 router.get("/", test);
-router.post("/register", upload.none(), registerUser);
+router.post("/register", upload.single("profileImage"), registerUser);
+router.post("/login", upload.none(), login);
+router.get("/logout", auth, logout);
+router.get("/get", auth, get);
+router.put("/edittext", auth, upload.none(), edittext);
+router.put("/editimage", auth, upload.single("profileImage"), editimage);
+router.put("/editpassword", auth, upload.none(), editpassword);
